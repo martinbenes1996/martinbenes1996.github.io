32a1cb96edbe11f4173f65a0a2778d34b8b2c665
Martin Benes <martinbenes1996@gmail.com>
2021-06-08 16:32:07 +0200

    xloc removed miscode

--- js/xloc.js
+++ js/xloc.js
@@ -66,15 +66,6 @@ function changeLang(){
     else { setCurrLang('EN') }
 }
 
-getJSON('http://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D%27WRC%27&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback',
-function(err, data) {
-  if (err !== null) {
-    alert('Something went wrong: ' + err);
-  } else {
-    alert('Your query count: ' + data.query.count);
-  }
-});
-
 // --- get translation ---
 function tr(key){
     return _tr[key]
