e4875fb336976e9e21ac81950eb2c2efaf276bac
Martin Benes <martinbenes1996@gmail.com>
2023-12-20 22:46:36 +0100

    guess probability

--- _posts/2023-12-20-sazka_lottery.md
+++ _posts/2023-12-20-sazka_lottery.md
@@ -15,24 +15,40 @@ tags:
 ---
 
 
+<a href="https://www.sazka.cz/sazka-svet/blog/jak-se-urcuje-vyhra-ve-sportce">Sazka Sportka</a>
+
+```
+# probabilities
+Pr_sportka <- dhyper(0:6, 6, 49-6, 6)
+Pr_dodatkove <- 1/(49-6)  # dodatkove cislo
+```
+
+| Matched         | Order | Probability       |
+| ----------------| ----- | ----------------- |
+| 6/6             | 1.    | $7.2\cdot10^{-8}$ |
+| 5/6 + dodatkové | 2.    | $4.3\cdot10^{-7}$ |
+| 5/6             | 3.    | $1.8\cdot10^{-5}$ |
+| 4/6             | 4.    | $9.7\cdot10^{-4}$ |
+| 3/6             | 5.    | $1.8\cdot10^{-2}$ |
+
+
+![guess_probability](img/sazka_lottery/guess_probability.png)
+
+
 <a href="https://www.sazka.cz/centrum-podpory/loterie-a-hry/loterie/sportka/jak-zjistit-vyhru-v-doplnkove-hre-sance">Sazka Šance</a>
 
-```R
-# probability of sance
 
+```
+# probability of sance
 Pr_sance <- 1/10^(1:6)
 Pr_sance <- c(1-sum(Pr_sance), Pr_sance)
+```
 
+```
 # reward of sance
-
-price_sance <- 20
 rewards_sance <- c(0, 50, 100, 10^3, 10^4, 10^5, 970000)
-avg_reward_sance <- Pr_sance %*% (rewards_sance - price_sance)
+avg_reward_sance <- Pr_sance %*% (rewards_sance - 20)
 avg_reward_sance
 ```
 
-
-
-
     -10
-
---
+++ img/sazka_lottery/code.R
@@ -0,0 +1,63 @@
+# https://www.sazka.cz/loterie/sportka/sazky-a-vysledky
+# https://www.sazka.cz/sazka-svet/blog/jak-se-urcuje-vyhra-ve-sportce
+# https://www.sazka.cz/centrum-podpory/loterie-a-hry/loterie/sportka/jak-zjistit-vyhru-v-doplnkove-hre-sance
+
+library(ggplot2)
+library(plyr)
+library(stats)
+
+# sance
+Pr_sance <- 1/10^(1:6)
+Pr_sance <- c(1-sum(Pr_sance), Pr_sance)
+rewards_sance <- c(0, 50, 100, 1000, 10000, 100000, 970000)
+avg_reward_sance <- Pr_sance %*% (rewards_sance - 20)
+
+# sportka
+Pr_sportka <- dhyper(0:6, 6, 49-6, 6)
+Pr_dodatkove <- 1/(49-6)  # dodatkove cislo
+Pr_sportka[6+1]  # 6/6 (1. poradi)
+Pr_sportka[5+1]*Pr_dodatkove # 5/6 + 1 (2. poradi)
+Pr_sportka[5+1]  # 5/6 (3. poradi)
+Pr_sportka[4+1]  # 4/6 (4. poradi)
+Pr_sportka[3+1]  # 3/6 (5. poradi)
+1 - sum(Pr_sportka[0:2+1])  # cokoli
+ggplot(data.frame(Uhodnuto=0:6, Pr=Pr_sportka)) +
+  geom_bar(aes(x=Uhodnuto, y=Pr_sportka), stat="identity", fill="steelblue") +
+  theme_minimal() +
+  scale_x_continuous(breaks=0:6) +
+  xlab("Numbers guessed") +
+  ylab("Probability")
+ggsave("guess_probability.png", width=8, height=4)
+ggplot(data.frame(`Vsazeno`=1:10, cols=1-sum(Pr_sportka[0:2+1])^(1:10))) +
+  geom_bar(aes(x=Vsazeno, y=cols), stat="identity", fill="steelblue") +
+  theme_minimal() +
+  xlab("Vsazeno sloupců") +
+  ylab("Pravděpodobnost jakékoli výhry") +
+  scale_x_continuous(breaks=1:10)
+reward_sportka_tah1 <- c(0, 0, 0, 108, 588, 23006, 393674)
+reward_sportka_tah2 <- c(0, 0, 0, 116, 645, 24491, 1000000)
+avg_reward_sportka <- (
+  # tah 1
+  Pr_sportka %*% (reward_sportka_tah1 - 20) +
+    Pr_sportka[5+1]*Pr_dodatkove*(700000 - 20) +
+    # tah 2
+    Pr_sportka %*% (reward_sportka_tah2) +
+    Pr_sportka[5+1]*Pr_dodatkove*(700000))
+
+# total reward
+avg_reward <- (
+  # tah 1+2
+  avg_reward_sportka +
+    # sance
+    avg_reward_sance +
+    # superjackpot
+    Pr_sportka[6+1]*Pr_sance[2]*(147000000 - 40) +
+    Pr_sportka[6+1]*Pr_sance[2]*(147000000))
+
+# sportka - simulation
+set.seed(12345)
+bet <- sample(1:49, 6, replace=F)
+win <- replicate(100000, length(intersect(sample(1:49, 6), bet)))
+table(win) / length(win)
+Pr_sportka
+
